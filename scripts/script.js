c449f9ed055257e10442b74069eb399b3a689c3d
JKoenen99 <jap.koenen@hotmail.nl>
2019-09-21 12:13:28 +0200

    Updated Dribbble Integration

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // Set the Access Token
-var accessToken = "aaaf79c846818aed133882b98352efaa9321da52d7cb8507bcb04de9bddba006"
+var accessToken = 'aaaf79c846818aed133882b98352efaa9321da52d7cb8507bcb04de9bddba006';
 
 // Call Dribble v2 API
 $.ajax({
@@ -9,12 +9,29 @@ $.ajax({
     success: function(data) {  
       if (data.length > 0) { 
         $.each(data.reverse(), function(i, val) {                
-          $('#dribbbleShots').prepend(
-            '<li class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></li>'
+          $('#shots').prepend(
+            '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
             )
         })
       }
       else {
-        $('#dribbbleShots').append('<p>Sorry, No shots yet</p>');
+        $('#shots').append('<p>No shots yet!</p>');
       }
     }
+});
+
+
+// API Variables
+
+//Image width 800x600 (Animated)
+  // val.images.hidpi 
+//Image width 400x300
+  // val.images.normal
+//Image width 200x150
+  // val.images.teaser
+//Title
+  // val.title
+//Description
+  // val.description
+//URL
+  // val.html_url
